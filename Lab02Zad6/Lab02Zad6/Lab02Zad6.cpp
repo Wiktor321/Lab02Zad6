d8ad01b4308b8016b14fb7d3c8d39819520fd87b
Wiktor321 <wiktortomaszek@gmail.com>
2021-12-10 23:02:19 +0100

    Adds an empty C++ project
    Empty Code::Blocks project

---
+++ Lab02Zad6/Lab02Zad6/Lab02Zad6.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+
+#include <windows.h>
+
+#include <time.h>
+using namespace std;
+
+
+int main()
+{
+	int liczba, n;
+
+	srand(time(NULL));
+
+	cout << "Podaj ilosc liczb do wylosowania: ", cin >> n;
+
+	for (int i = 0; i <= n; i++)
+	{
+		liczba = rand() % 56 - 10;
+
+		cout << liczba << " ";
+	}
+	cout << endl << endl;
+
+	int suma = 0;
+
+	for (int j = -10; j < 45; j += 2)
+
+	{
+		suma += j;
+	}
+	cout << "Suma parzystych elemntow tego ciagu jest rowna = " << suma;
+
+	return 0;
+}
+
+
